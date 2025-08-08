a0e899e6d24b7a6a1afed68ebc058c9fb3cc4a97
Jamilly <jamillyrodriguesfroes@gmail.com>
2025-08-08 17:11:16 -0300

    mais telas

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ if($_SERVER['REQUEST_METHOD']== "POST"){
 
     $sql = "SELECT * FROM usuario WHERE email = :email";
     $stmt= $pdo->prepare($sql);
-    $stmt->bindParam('email', $email);
+    $stmt->bindParam(':email', $email);
     $stmt->execute();
     $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
 
--- recuperar_senha.php
+++ recuperar_senha.php
@@ -28,9 +28,26 @@ if($_SERVER["REQUEST_METHOD"]== "POST"){
         //simula o envio do email (grava em txt)
         simularEnvioEmail($email,$senha_temporaria);
         echo "<script>alert('Uma senha temporaria foi gerada e enviada(simulação). Verique o arquivo emails_simulados.txt'); window.location.href='login.php';</script>";
+    }else{
+        echo "<script>alert('E-mail não encontrado!');</script>";
     }
-
-
 }
-
 ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recuperar a Senha</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <h2>Recuperar Senha</h2>
+    <form action="recuperar_senha.php" method="POST">
+        <label for="email">Digite o seu email cadastrado</label>
+        <input type="email" id="emai" name="email" required>
+
+        <button type="submit">Enviar a Senha Temporaria</button>
+    </form>
+</body>
+</html>
