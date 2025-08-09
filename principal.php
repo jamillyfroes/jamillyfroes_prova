9a8cb822f248af87dd7ddf3f7e50d1490ae69ee5
Jamilly <jamillyrodriguesfroes@gmail.com>
2025-08-09 19:11:48 -0300

    ajustes
    arrumando os cochetes

--- principal.php
+++ principal.php
@@ -19,7 +19,7 @@ $nome_perfil= $perfil['nome_perfil'];
 //definição das permissoes por perfil
 
 $permissoes=[
-    1 =>["Cadastrar"=>["cadastro_usuario.php","cadastro_perfil.php","cadastro_cliente.php","cadastro_fornecedor.php","cadastro_produto.php","cadastro_funcionario.php"]],
+    1 =>["Cadastrar"=>["cadastro_usuario.php","cadastro_perfil.php","cadastro_cliente.php","cadastro_fornecedor.php","cadastro_produto.php","cadastro_funcionario.php"],
     "Buscar"=>["buscar_usuario.php","buscar_perfil.php","buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php","buscar_funcionario.php"],
     "Alterar"=>["alterar_usuario.php","alterar_perfil.php","alterar_cliente.php","alterar_fornecedor.php","alterar_produto.php","alterar_funcionario.php"],
     "Excluir"=>["excluir_usuario.php","excluir_perfil.php","excluir_cliente.php","excluir_fornecedor.php","excluir_produto.php","excluir_funcionario.php"]],
@@ -27,9 +27,6 @@ $permissoes=[
     2 =>["Cadastrar"=>["cadastro_cliente.php"]],
     "Buscar"=>["buscar_cliente.php","buscar_fornecedor.php","buscar_produto.php"],
     "Alterar"=>["alterar_fornecedor.php","alterar_produto.php"],
-    "Excluir"=>["excluir_produto.php"]],
-
-    
-
-]
+    "Excluir"=>["excluir_produto.php"],
+    ]
 ?>
