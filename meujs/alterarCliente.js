ee6b579731fe0a738788c4e0fc7f037ba356e560
Jamilly <jamillyrodriguesfroes@gmail.com>
2025-08-21 16:50:44 -0300

    JS Alterar Clientes
    Validações do campo funcionando!

---
+++ meujs/alterarCliente.js
@@ -0,0 +1,71 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form[action='processa_alteracao_cliente.php']");
+
+    if (form) {
+        form.addEventListener("submit", function (event) {
+            let nome = document.getElementById("nome").value.trim();
+            let endereco = document.getElementById("endereco").value.trim();
+            let telefone = document.getElementById("telefone").value.trim();
+            let email = document.getElementById("email").value.trim(); 
+            let funcionario_responsavel = document.getElementById("id_funcionario_responsavel").value;
+
+            //nome
+            if (nome === "") {
+                alert("O campo Nome deve ser preenchido!");
+                document.getElementById("nome").focus();
+                event.preventDefault();
+                return;
+            }
+            if (nome.length < 3) {
+                alert("O nome deve ter pelo menos 3 caracteres.");
+                document.getElementById("nome").focus();
+                event.preventDefault();
+                return;
+            }
+
+             //valida endereco
+        if (endereco === "") {
+            alert("O campo Endereço deve ser preenchido!");
+            document.getElementById("endereco").focus();
+            event.preventDefault();
+            return;
+        }
+
+        
+        if (endereco.length < 3) {
+            alert("O Endereço deve ter pelo menos 3 caracteres.");
+            document.getElementById("endereco").focus();
+            event.preventDefault();
+            return;
+        }
+
+          //valida telefone
+          if (telefone === "") {
+            alert("O campo Telefone deve ser preenchido!");
+            document.getElementById("endereco").focus();
+            event.preventDefault();
+            return;
+        }
+
+            //email
+            let regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!regexEmail.test(email)) {
+                alert("Digite um e-mail válido.");
+                document.getElementById("email").focus();
+                event.preventDefault();
+                return;
+            }
+
+         
+        // valida funcionario responsavel
+        if (funcionario_responsavel === "") {
+            alert("Selecione um Funcionario Responsável.");
+            document.getElementById("id_funcionario_responsavel").focus();
+            event.preventDefault();
+            return;
+            }
+
+        });
+    }
+});
